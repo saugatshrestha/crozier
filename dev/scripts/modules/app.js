3b0b4920cc0b11822a97c862496bece5878b5dfc
saugatshrestha <saugatshrestha28@gmail.com>
2022-07-25 15:37:26 +0545

    styleguide updated

--- dev/scripts/modules/app.js
+++ dev/scripts/modules/app.js
@@ -11,55 +11,7 @@ import 'bootstrap/js/dist/modal';
 
 class global {
     constructor() {
-        this.myModal = document.getElementById('exampleModal')
-        this.stickyHeader();
-        this.megamenuPopup();
-        this.toggleContent();
-        this.readMore();
-    }
-
-    stickyHeader() {
-        var elemBody = $('body');
-        var pageHeader = $('.theme-header');
-        if ($(elemBody).hasClass('home')) {
-            $(pageHeader).addClass('theme-header--dark');
-        }
-
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('.theme-header').addClass('sticky');
-            } else {
-                $('.theme-header').removeClass('sticky');
-            }
-        });
-    }
-
-    megamenuPopup() {
-        var toggleBar = $('.navbar-toggle');
-        var popupMenu = $('.megamenu-popup');
-        var closePopup = $('.close-menu');
-        $(toggleBar).click(function () {
-            $(popupMenu).addClass('active');
-        });
-        $(closePopup).click(function () {
-            $(popupMenu).removeClass('active');
-        });
-    }
-
-    toggleContent() {
-        var toggleContent = $('.toggle-text');
-        var viewBtn = $('.toggleRead');
-        $(viewBtn).click(function () {
-            $(this).toggleClass('active');
-            $(toggleContent).toggleClass('active');
-        });
-    }
-
-    readMore() {
-        $('.readmore').click(function () {
-            $('.collapse-text').addClass('expand');
-            $(this).hide();
-        });
+        
     }
 
 }
